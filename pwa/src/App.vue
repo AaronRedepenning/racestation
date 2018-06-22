994b970ff3b95478b77828440424edec5573aefb
Aaron Redepenning <aaronredepenning@live.com>
2018-06-21 22:36:14 -0500

    Initial Commit

---
+++ pwa/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app id="racestation">
+    <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-tile :key="item.text">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="light-blue" dark app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Racestation</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>view_module</v-icon>
+        </v-btn>
+      </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <!-- Page Content -->
+          <router-view></router-view>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'cloud', text: 'Weather' }
+      ]
+    })
+  }
+</script>
---
+++ pwa/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-layout column>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-subheader>Overview</v-subheader>
+          <v-divider></v-divider>
+          72.2 °F
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-list>
+            <v-subheader>Details</v-subheader>
+            <v-divider></v-divider>
+            <v-list-tile v-for="item in items" :key="item.name">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                {{ item.value }}
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { name: 'Absolute Temperature', value: "1009.2 hPa" },
+          { name: 'Air Temperature', value: "72.2 °F" },
+          { name: 'Relative Humidity', value: "22 %" },
+          { name: 'Adjusted Altitude', value: "60 Ft" },
+          { name: 'Absolute Humidity', value: "40 %" },
+          { name: 'Grains', value: "99" },
+          { name: 'Vapor Pressure', value: "300.9 bars" },
+          { name: 'Dew Point', value: "55 °F" },
+          { name: 'Air Density Ratio', value: "33" },
+          { name: 'Density Altitude', value: "70 Ft" },
+          { name: 'Pressure Altitude', value: "44 Ft" },
+          { name: 'Wind Speed', value: "12 mph" },
+          { name: 'Wind Direction', value: "NW" },
+          { name: 'Wind Gust', value: "30 mph" },
+          { name: 'Ambient Light', value: "30 lumens" }
+        ]
+      }
+    }
+  }
+</script>
